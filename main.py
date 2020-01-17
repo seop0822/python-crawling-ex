804b7c60a6da68edc506ed07165d6855374df173
Yun Seop Park <seop0822@naver.com>
2020-01-17 23:43:03 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+indeed_result = requests.get("https://www.indeed.com/jobs?q=python&limit=50")
+
+indeed_soup = BeautifulSoup(indeed_result.text, 'html.parser')
+
+pagination = indeed_soup.find("div", {"class": "pagination"})
+
+pages = pagination.find_all("a")
+
+
+spans = []
+for page in pages:
+  spans.append(page.find("span"))
+
+spans = spans[:-1]
+print(spans)
